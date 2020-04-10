1bcce6c0c3b5aaeb3c90099a42f2388df483cc96
Daniel <Daniel@192.168.0.142>
2020-04-10 12:01:52 -0400

    Flare Fire sound on break working.

--- Abyssal/src/main/java/djknarnia/abyssal/events/LeftClickEvent.java
+++ Abyssal/src/main/java/djknarnia/abyssal/events/LeftClickEvent.java
@@ -4,7 +4,11 @@ import djknarnia.abyssal.abyssal;
 import lists.BlockList;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.world.World;
+import net.minecraftforge.client.event.sound.SoundEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -25,6 +29,13 @@ public class LeftClickEvent {
 		if(clickedBlock == BlockList.flare_fire) {
 			world.setBlockState(event.getPos().offset(event.getFace()), Blocks.AIR.getDefaultState());
 			
+			double x = event.getPos().getX();
+			double y = event.getPos().getY();
+			double z = event.getPos().getZ();
+			
+			SoundCategory soundCategory = SoundCategory.BLOCKS;
+			
+			world.playSound(x, y, z, SoundEvents.BLOCK_FIRE_EXTINGUISH, soundCategory, 1.0f, 1.0f, false);
 		}
 	}
 }
